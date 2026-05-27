9233d4a18d53bc1e25afa65b7677e70c7bfe30f1
Tumelo-create <sba25142@student.cct.ie>
2026-05-27 01:00:47 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
 from mlxtend.frequent_patterns import apriori, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 
