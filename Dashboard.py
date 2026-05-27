a48acfc726abc67430463ad125759863eb523d2a
Tumelo-create <sba25142@student.cct.ie>
2026-05-27 01:06:44 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -22,11 +22,6 @@ Design for adults 65+:
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-from mlxtend.frequent_patterns import apriori, association_rules
-from mlxtend.preprocessing import TransactionEncoder
-
 # ------------------------------
 # 1. PAGE CONFIGURATION (senior-friendly)
 # ------------------------------
